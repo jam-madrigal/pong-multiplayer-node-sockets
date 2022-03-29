7854ef10f2b4920486f4420e1d5c099fb5d8b4a2
jam-madrigal <jam.madrigal@gmail.com>
2022-03-28 23:04:13 -0700

    Add server with socket.io and a basic connection event listener

---
+++ server.js
@@ -0,0 +1,13 @@
+const server = require('http').createServer();
+// Importing socket.io and attaching it to our server
+const io = require('socket.io')(server);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}...`);
+})
+
+// Registering an event listener for the connection event, which happens every time a user connects
+io.on('connection', (socket) => {
+    console.log('User connected');
+});
