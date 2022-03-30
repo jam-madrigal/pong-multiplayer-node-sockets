07094b7e53716c9821200228bd7750a8d3eefb6d
jam-madrigal <jam.madrigal@gmail.com>
2022-03-29 20:16:35 -0700

    Add increment for player count and listener to start the game

--- server.js
+++ server.js
@@ -12,7 +12,21 @@ server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}...`);
 });
 
+// Variable for tracking how many players are ready
+let readyPlayerCount = 0;
+
 // Registering an event listener for the connection event, which happens every time a user connects, and logging the user's socket id
 io.on('connection', (socket) => {
     console.log('User connected', socket.id);
+    // Listening for a user to connect and be ready, then incrementing our ready player count each time
+    socket.on('ready', () => {
+        console.log('Player ready', socket.id);
+
+        readyPlayerCount++;
+        // Broadcast the start game event when both players are ready, and send the player referee id, which will always be the second player in this case
+        if (readyPlayerCount === 2) {
+            io.emit('startGame', socket.id);
+        }
+        
+    });
 });
