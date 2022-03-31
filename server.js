61b65200c8505a2a94b6f58b28f9f797d2cf26ea
jam-madrigal <jam.madrigal@gmail.com>
2022-03-30 23:57:11 -0700

    Update comments

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const api = require('./api');
-
+// Passing in our express function to make it listen to requests on the server
 const server = require('http').createServer(api);
 // Importing socket.io and attaching it to our server
 const io = require('socket.io')(server, {
@@ -17,5 +17,6 @@ server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}...`);
 });
 
+// Server is listening to events in our sockets file
 sockets.listen(io);
 
