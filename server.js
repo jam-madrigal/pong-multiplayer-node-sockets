aff8ba4cd92e1aecc767902e9c08ac628c9c1a16
jam-madrigal <jam.madrigal@gmail.com>
2022-03-31 00:02:03 -0700

    Clean imports

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const http = require('http');
+const io = require('socket.io');
 const api = require('./api');
 // Passing in our express function to make it listen to requests on the server
-const server = require('http').createServer(api);
+const httpServer = http.createServer(api);
 // Importing socket.io and attaching it to our server
-const io = require('socket.io')(server, {
+const socketServer = io(httpServer, {
     cors: {
       origin: '*',
       methods: ['GET', 'POST']
@@ -13,10 +15,10 @@ const sockets = require('./sockets');
 
 const PORT = 3000;
 
-server.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}...`);
 });
 
 // Server is listening to events in our sockets file
-sockets.listen(io);
+sockets.listen(socketServer);
 
